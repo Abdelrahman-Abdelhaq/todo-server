17292e088390379e699b30fec2c8c8e414f2d558
Abdelrahman Alaa <abdelrahmanalaa.psn@gmail.com>
2025-08-03 12:06:24 +0300

    first try to use neon

--- db.js
+++ db.js
@@ -1,9 +1,6 @@
 import {Pool} from 'pg';
 
 export const pool = new Pool({
-    user:"postgres",
-    password:"0707",
-    host:"localhost",
-    port:5432,
-    database:"todo" 
+    connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false } 
 })
