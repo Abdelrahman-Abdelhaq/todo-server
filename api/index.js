eca5b08ba15252569860daf249869e6da504fbaf
Abdelrahman Alaa <abdelrahmanalaa.psn@gmail.com>
2025-09-29 13:49:34 +0300

    vercel fix

--- api/index.js
+++ api/index.js
@@ -1,3 +1,16 @@
-import app from "../server"
+import express from "express";
+import cors from "cors";
+import { allNotes, oneNote, addNote, updateNote, deleteNote } from "../controllers.js"; 
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/notes", allNotes);
+app.get("/notes/:id", oneNote);
+app.post("/notes", addNote);
+app.put("/notes/:id", updateNote);
+app.delete("/notes/:id", deleteNote);
 
 export default app;
