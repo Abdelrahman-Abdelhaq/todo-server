f5b3a27dc150af1efb18ddf506530c593b507d14
Abdelrahman Alaa <abdelrahmanalaa.psn@gmail.com>
2025-09-29 13:40:48 +0300

    vercel deployment

--- server.js
+++ server.js
@@ -1,27 +1,17 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import cors from 'cors';
-import {pool} from './db.js';
-import { addNote, allNotes, deleteNote, oneNote, updateNote } from './controllers.js';
+import express from "express";
+import cors from "cors";
+import { allNotes, oneNote, addNote, updateNote, deleteNote } from "../controllers.js"; 
+import { pool } from "../db.js"; 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173'
-}));
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/notes', allNotes);
+app.get("/notes", allNotes);
+app.get("/notes/:id", oneNote);
+app.post("/notes", addNote);
+app.put("/notes/:id", updateNote);
+app.delete("/notes/:id", deleteNote);
 
-app.get('/notes/:id', oneNote);
-
-app.post('/notes', addNote);
-
-app.put('/notes/:id', updateNote);
-
-app.delete('/notes/:id', deleteNote);
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+export default app;
